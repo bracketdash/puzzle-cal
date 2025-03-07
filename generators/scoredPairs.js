141f624a3bccb568e42f813e17459d8e48a1e640
Michael Hatch <michaelhatch@me.com>
2025-03-07 15:07:25 -0800

    write scroedPairs up to difference scoring

--- generators/scoredPairs.js
+++ generators/scoredPairs.js
@@ -8,25 +8,22 @@ const validDates = JSON.parse(
   fs.readFileSync(new URL("../generated/validDates.json", import.meta.url))
 );
 
-/*
-for each valid date in the year (except dec 31) (allow flag to set whether it's a leap year):
-    for each of this day's solutions:
-        for each of the next day's solutions:
-            create a difference score for the pair
+const scoredPairs = {};
 
-resulting data should look like...
-
-{
-    'JAN1-JAN2': {
-        '0-0': 6, // this means we are comparing JAN1's first solution to JAN2's first solution, and its difference score is 6 (made up)
-        '0-1': 4,
-    },
-    'JAN2-JAN3': {
-        ...
-    },
-    ...
-}
-*/
-
-console.log(Object.keys(solutions).length); // 372
-console.log(validDates.length); // 365
+validDates.slice(0, -1).forEach((firstDate, firstDateIndex) => {
+  solutions[firstDate].forEach((firstDateSolution, firstDateSolutionIndex) => {
+    const secondDate = validDates[firstDateIndex + 1];
+    solutions[secondDate].forEach(
+      (secondDateSolution, secondDateSolutionIndex) => {
+        const dateKey = `${firstDate}-${secondDate}`;
+        const pairKey = `${firstDateSolutionIndex}-${secondDateSolutionIndex}`;
+        if (!scoredPairs[dateKey]) {
+          scoredPairs[dateKey] = {};
+        }
+        let score = 0;
+        // TODO: Create a difference score between `firstDateSolution` and `secondDateSolution`
+        scoredPairs[dateKey][pairKey] = score;
+      }
+    );
+  });
+});
