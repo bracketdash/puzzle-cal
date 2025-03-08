1bd4b97ead08c43d3eb1acfe0e9fbadabed36c43
Michael Hatch <michaelhatch@me.com>
2025-03-08 15:18:59 -0800

    different strat; need efficiency for my whittle cpu

--- generators/bestPath.js
+++ generators/bestPath.js
@@ -1,16 +1,50 @@
 import fs from "fs";
 
-const scoredPairs = JSON.parse(
-  fs.readFileSync(new URL("../generated/scoredPairs.json", import.meta.url))
+const solutions = JSON.parse(
+  fs.readFileSync(new URL("../generated/allSolutions.json", import.meta.url))
 );
 
-const dayPairs = Object.keys(scoredPairs);
+const validDates = JSON.parse(
+  fs.readFileSync(new URL("../generated/validDates.json", import.meta.url))
+);
+
+const scoredPairs = {};
 
 let bestPath = [];
 let lowestScore = Infinity;
 
-function findOptimalPath(path = [], score = 0, a = 0, b = 0) {
-  const dayPairKey = dayPairs[path.length];
+function getScoredPairs(firstDateIndex) {
+  const firstDate = validDates[firstDateIndex];
+  const secondDate = validDates[firstDateIndex + 1];
+  const dateKey = `${firstDate}-${secondDate}`;
+  if (scoredPairs[dateKey]) {
+    return scoredPairs[dateKey];
+  }
+  scoredPairs[dateKey] = [];
+  solutions[firstDate].forEach((firstDateSolution, firstDateSolutionIndex) => {
+    solutions[secondDate].forEach(
+      (secondDateSolution, secondDateSolutionIndex) => {
+        let score = 0;
+        for (let i = 0; i < 43; i++) {
+          if (firstDateSolution[i] !== secondDateSolution[i]) {
+            score++;
+          }
+        }
+        scoredPairs[dateKey].push([
+          firstDateSolutionIndex,
+          secondDateSolutionIndex,
+          score,
+        ]);
+      }
+    );
+  });
+  scoredPairs[dateKey] = scoredPairs[dateKey].sort((a, b) => a[2] - b[2]);
+  return scoredPairs[dateKey];
+}
+
+// TODO: adapt to use above function instead of relying on readymade scored pairs
+function findBestPath(path = [], score = 0, a = 0, b = 0) {
+  const dayPairKey = validDates[path.length];
   const dayPairScores = scoredPairs[dayPairKey];
   if (a >= dayPairScores.length || b >= dayPairScores[a].length) {
     return;
@@ -20,7 +54,7 @@ function findOptimalPath(path = [], score = 0, a = 0, b = 0) {
     return;
   }
   const newPath = [...path, [a, b]];
-  if (newPath.length === dayPairs.length) {
+  if (newPath.length === validDates.length) {
     if (newScore < lowestScore) {
       lowestScore = newScore;
       bestPath = JSON.parse(JSON.stringify(newPath));
@@ -32,16 +66,14 @@ function findOptimalPath(path = [], score = 0, a = 0, b = 0) {
     }
     return;
   }
-  const nextDayPairKey = dayPairs[newPath.length];
+  const nextDayPairKey = validDates[newPath.length];
   const nextDayPairScores = scoredPairs[nextDayPairKey];
   const lastB = b;
   if (lastB < nextDayPairScores.length) {
     for (let nextB = 0; nextB < nextDayPairScores[lastB].length; nextB++) {
-      findOptimalPath(newPath, newScore, lastB, nextB);
+      findBestPath(newPath, newScore, lastB, nextB);
     }
   }
 }
 
-findOptimalPath();
-console.log("Final best path:", bestPath);
-console.log("Lowest score:", lowestScore);
+findBestPath();
